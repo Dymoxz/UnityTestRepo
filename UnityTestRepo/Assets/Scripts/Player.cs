d9e889010a4d0c453713dc565465d86c36c4b117
Mech <ss.rensma@student.avans.nl>
2026-05-09 15:01:46 +0200

    Revert "hlep"
    This reverts commit 6a25800e15c40d71fcca04a98f46c6aec7af1876.

--- UnityTestRepo/Assets/Scripts/Player.cs
+++ UnityTestRepo/Assets/Scripts/Player.cs
@@ -1,25 +1,16 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
 
-    public Rigidbody2D rb;
-    public float jumpAmount;
-
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
-
-    private bool isGrounded;
-
+    // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
-        }
+        
     }
 }
